953225ad0b4663d2d4b0c17168c853a922b801f8
Manoj-Bittu <knmanoj2@gmail.com>
2021-04-07 09:43:00 +0530

    added

--- src/day_17_Pop_upss_03/Demo_FileDownload.java
+++ src/day_17_Pop_upss_03/Demo_FileDownload.java
@@ -1,10 +1,24 @@
 package day_17_Pop_upss_03;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+
 public class Demo_FileDownload {
 
 	public static void main(String[] args) {
+		FirefoxProfile prof = new FirefoxProfile();
+		prof.setPreference("browser.helperApps.neverAsk.saveToDisk", "application/java-archive");
 		
-
+		WebDriver driver = new FirefoxDriver(prof);
+		driver.manage().window().maximize();
+		driver.get("https://krninformatix.com/download.php");
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		
+		driver.findElement(By.xpath("(//a[text()='Click Here'])[2]")).click();
 	}
 
 }
